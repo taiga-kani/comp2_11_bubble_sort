93f67a2792260ea50054d49dabfb6db06020e6c6
Taiga Miura <taigamiura1623@gmail.com>
2026-01-05 03:52:22 +0900

    テストを通しました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,36 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+    if (begin == NULL || end == NULL)
+    {
+        return false;
+    }
+
+    if (begin >= end)
+    {
+        return false;
+    }
+
+    size_t count = end - begin;
+
+    if (count < 2)
+    {
+        return true;
+    }
+
+    for (size_t i = 0; i < count - 1; ++i)
+    {
+        for (size_t j = 0; j < count - 1 - i; ++j)
+        {
+            if (begin[j].key > begin[j + 1].key)
+            {
+                item tmp;
+                tmp = begin[j];
+                begin[j] = begin[j + 1];
+                begin[j + 1] = tmp;
+            }
+        }
+    }
+
+    return true;
 }
